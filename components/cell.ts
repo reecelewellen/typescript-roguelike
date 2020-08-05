c9571a79b172f5c452c7f615780b297f0061d331
Reece Lewellen <reece.lewellen@gmail.com>
2020-08-05 16:10:53 -0500

    updated ignore compiled *.js files

--- components/cell.ts
+++ components/cell.ts
@@ -1,22 +1,27 @@
-const terrain_symbols = ['┌',
-'─',
-'┐',
-'│',
-'┘',
-'─',
-'└',
-'│',
-'.',
-'▒',
+const terrain_symbols = [
+  '┌',
+  '─',
+  '┐',
+  '│',
+  '┘',
+  '─',
+  '└',
+  '│',
+  '.',
+  '▒',
 ]
 
 export class Cell {
-  pos_x = 0;
-  pos_y = 0;
-  contains: string = null;
+  pos_x:number;
+  pos_y:number;
+  contains: string;
   constructor(x: number, y: number, wall_type: number) {
     this.pos_x = x;
     this.pos_y = y;
+    this.contains = terrain_symbols[wall_type];
+  }
+
+  setContents = (wall_type): void => {
     this.contains = terrain_symbols[wall_type];
   }
 
